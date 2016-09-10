d2a937674447167d9a9db49ad6c47013103f87de
Kalle0x12 <Kalle0x12@users.noreply.github.com>
2016-09-10 21:58:26 +0200

    Travis: LIS: disable longdouble

--- install-lis.sh
+++ install-lis.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-#set -ex
+#!/bin/bash
+set -ex
 wget http://www.ssisc.org/lis/dl/lis-1.6.6.tar.gz
 tar -xzvf lis-1.6.6.tar.gz
 patch -p0 < lis.patch
-cd lis-1.6.6 && ./configure --prefix=/usr/local/lis --enable-omp  --enable-shared --enable-longdouble && make -j2 && sudo make install
+cd lis-1.6.6 && ./configure --prefix=/usr/local/lis --enable-omp  --enable-shared  && make -j2 && sudo make install
 
