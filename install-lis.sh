1c44a6e1f290c181ffd1fd3d757a0f8a821472ec
Kalle0x12 <Kalle0x12@users.noreply.github.com>
2016-09-10 21:43:46 +0200

    Travis: Fix LIS patch

--- install-lis.sh
+++ install-lis.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-set -ex
+#set -ex
 wget http://www.ssisc.org/lis/dl/lis-1.6.6.tar.gz
 tar -xzvf lis-1.6.6.tar.gz
+patch -p0 < lis.patch
 cd lis-1.6.6 && ./configure --prefix=/usr/local/lis --enable-omp  --enable-shared --enable-longdouble && make -j2 && sudo make install
 
