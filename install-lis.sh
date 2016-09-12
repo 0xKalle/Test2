a7d04e0e94b3b7435747ed477f5e40de8cc36837
Kalle0x12 <Kalle0x12@users.noreply.github.com>
2016-09-12 17:37:19 +0200

    Travis: install LD_LIBRARY_PATH docker

--- install-lis.sh
+++ install-lis.sh
@@ -4,4 +4,5 @@ wget http://www.ssisc.org/lis/dl/lis-1.6.6.tar.gz
 tar -xzvf lis-1.6.6.tar.gz
 patch -p0 < lis.patch
 cd lis-1.6.6 && ./configure --prefix=$HOME/lis --enable-omp  --enable-shared  && make -j2 && make install
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lis/lib
 
