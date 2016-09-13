4702c65c3466520fbdbe3f3bb4f400ca54fc566e
Kalle0x12 <Kalle0x12@users.noreply.github.com>
2016-09-13 18:50:49 +0200

    Install pybind11 with pytest

--- install-lis.sh
+++ install-lis.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -ex
-wget http://www.ssisc.org/lis/dl/lis-1.6.6.tar.gz
-tar -xzvf lis-1.6.6.tar.gz
+git clone https://github.com/anishida/lis.git
 patch -p0 < lis.patch
-cd lis-1.6.6 && ./configure --prefix=$HOME/lis --enable-omp  --enable-shared  && make -j2 && make install
-
-
+cd lis && ./configure --prefix=$HOME/lis --enable-omp  --enable-shared  && make -j2 && make check && make install
--- install-pybind11.sh
+++ install-pybind11.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -ex
 git clone https://github.com/pybind/pybind11.git
-#last working version
-#wget https://github.com/pybind/pybind11/archive/29b5064e9c80be7034fc9d6ac15dbb7ebe304fc5.zip
-#unzip 29b5064e9c80be7034fc9d6ac15dbb7ebe304fc5.zip
-#ln -s pybind11-29b5064e9c80be7034fc9d6ac15dbb7ebe304fc5 pybind11
 cd pybind11
 cmake  -DPYBIND11_PYTHON_VERSION=2.7 -DPYBIND11_CPP_STANDARD=-std=c++14 -DPYBIND11_WERROR=OFF
 make -j2 pytest
