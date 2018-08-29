3fcf5fd78a5242eae26f3766fd7348ff3a6ebc20
Kalle <kalle@localhost>
2018-08-29 21:12:24 +0200

    fix dirname

--- install-pybind11.sh
+++ install-pybind11.sh
@@ -3,7 +3,8 @@ set -ex
 #git clone https://github.com/pybind/pybind11.git
 #cd pybind11
 wget https://github.com/pybind/pybind11/archive/v2.2.3.tar.gz
-tar xf v2.2.3.tar.gz
-cd pybind11-2.2.3
+mkdir pybind11
+tar xf v2.2.3.tar.gz -C pybind11 --strip 1
+cd pybind11
 cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 $3 -DPYBIND11_WERROR=OFF
 make -j2 pytest
