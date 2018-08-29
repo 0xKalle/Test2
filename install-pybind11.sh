e0d0784536659f98313f61f9157bef7f20006ff3
Kalle <kalle@localhost>
2018-08-29 19:53:23 +0200

    extract archive

--- install-pybind11.sh
+++ install-pybind11.sh
@@ -3,6 +3,7 @@ set -ex
 #git clone https://github.com/pybind/pybind11.git
 #cd pybind11
 wget https://github.com/pybind/pybind11/archive/v2.2.3.tar.gz
+tar xf v2.2.3.tar.gz
 cd pybind11-2.2.3
 cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 $3 -DPYBIND11_WERROR=OFF
 make -j2 pytest
