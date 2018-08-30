8c6039c902c3bb4991ee5cfbf99153efb73df52e
Kalle <kalle@localhost>
2018-08-30 13:04:32 +0200

    fix cmake parms build-script pybind11

--- install-pybind11.sh
+++ install-pybind11.sh
@@ -6,5 +6,6 @@ wget https://github.com/pybind/pybind11/archive/v2.2.3.tar.gz
 mkdir pybind11
 tar xf v2.2.3.tar.gz -C pybind11 --strip 1
 cd pybind11
-cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 $3 -DPYBIND11_WERROR=OFF
+export CMAKE_INCLUDE_PATH=$4
+cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 $3 -DPYBIND11_WERROR=OFF -DDOWNLOAD_CATCH=ON
 make -j2 pytest
