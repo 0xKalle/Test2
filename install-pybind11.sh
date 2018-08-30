0f5a1670cc31ba56a458a9dc2a9a96f2051ba348
Kalle <kalle@localhost>
2018-08-30 11:35:53 +0200

    cleanup cppv/cpp

--- install-pybind11.sh
+++ install-pybind11.sh
@@ -6,5 +6,5 @@ wget https://github.com/pybind/pybind11/archive/v2.2.3.tar.gz
 mkdir pybind11
 tar xf v2.2.3.tar.gz -C pybind11 --strip 1
 cd pybind11
-cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 -DCMAKE_EXTRA_ARGS=$3 -DCMAKE_INCLUDE_PATH=$4 -DPYBIND11_WERROR=OFF
+cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 $3 -DCMAKE_INCLUDE_PATH=$4 -DPYBIND11_WERROR=OFF
 make -j2 pytest
